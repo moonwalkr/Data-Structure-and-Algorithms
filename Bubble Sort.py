56151a00589e5783c92b78a750c3543d0dcda214
moonwalkr <57403069+moonwalkr@users.noreply.github.com>
2020-02-23 21:57:33 -0600

    Bubble Sort Implementation

---
+++ Bubble Sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+
+    for n in range(len(arr) - 1, 0, -1):
+
+        for k in range(n):
+
+            if arr[k] > arr[k + 1]:
+                temp = arr[k]
+                arr[k] = arr[k + 1]
+                arr[k + 1] = temp
